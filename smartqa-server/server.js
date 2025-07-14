027a32e0b8b1012fdee9267cb46d39dc8d5deab1
ruex64 <workwithruex64@gmail.com>
2025-07-14 15:53:42 +0530

    server

--- smartqa-server/server.js
+++ smartqa-server/server.js
@@ -1,28 +1,35 @@
-require ('dotenv').config();
-const express= require('express');
+require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose')
+const roomRoutes = require('./src/routes/roomRoutes')
 
-const app=express();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("Mongo DB Connected"))
+    .catch((error) => console.log("Error in connecting DB", error));
 
 
 //middlewares
 app.use(express.json());
 
-const corsConfig={
-    origin:process.env.CLIENT_URL,
-    credentials:true
+const corsConfig = {
+    origin: process.env.CLIENT_URL,
+    credentials: true
 };
 
 app.use(cors(corsConfig));
 
 //start server
-const Port=process.env.PORT;
+const Port = process.env.PORT;
 
-app.listen(Port,(error)=>{
-    if(error){
-        console.log("Error is there.Server not starting due to ",error);
+app.listen(Port, (error) => {
+    if (error) {
+        console.log("Error is there.Server not starting due to ", error);
     }
-    else{
-        console.log("Server started at port:",Port);
+    else {
+        console.log("Server started at port:", Port);
     }
 });
