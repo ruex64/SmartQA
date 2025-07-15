d88e090cb68bfb41482c6d5e19d84ba4ce153ab9
ruex64 <workwithruex64@gmail.com>
2025-07-15 14:05:56 +0530

    upd

--- smartqa-server/src/models/User.js
+++ smartqa-server/src/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String },
   role: { type: String, enum: ["user", "admin"], default: "user" },
-  googleId: { type: String }, // For OAuth
+  googleId: { type: String },
 });
 
 module.exports = mongoose.model("User", userSchema);
