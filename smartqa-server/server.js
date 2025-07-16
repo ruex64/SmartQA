93d1530d2ab4cf3230dd85d428a82f176d4de078
ruex64 <workwithruex64@gmail.com>
2025-07-16 15:40:28 +0530

    added socket io

--- smartqa-server/server.js
+++ smartqa-server/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const roomRoutes = require('./src/routes/roomRoutes');
+
 
 const app = express(); // Create instance of express to setup the server
 
@@ -18,6 +21,30 @@ const corsConfig = {
     credentials: true
 };
 app.use(cors(corsConfig));
+
+const OurServer = http.createServer(app);
+
+const io = new Server(OurServer, {
+    cors: {
+        origin: process.env.CLIENT_URL,
+        methods: ["GET", "POST", "DELETE", "UPDATE"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log('New Client connection: ', socket.id);
+
+    socket.on("join-room", (roomCode) => {
+        socket.join(roomCode);
+        console.log(`User joined room: ${roomCode}`);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnection: ", socket.id);
+    });
+});
+
+app.set("io",io);
 
 app.use('/room', roomRoutes);
 
